3a235281f12579ee31a4ee9ad0737dafcf698543
alimurrofid <alimurrofid77@gmail.com>
2023-07-30 14:28:56 +0700

    feat:bootstrap vendor

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <!-- Vendor CSS Files -->
+
+     <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+     <link href="{{asset('assets/vendor/bootstrap-icons-1.10.5/font/bootstrap-icons.css')}}" rel="stylesheet">
+ 
+     <!-- CSS Style -->
+     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+ 
+     <link rel="icon" href="{{ asset('assets/img/logo-formadiksi.png') }} " type="image/x-icon" />
+    <title>Official Formadiksi</title>
+</head>
+<body>
+    
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+
+    <!-- Main JS File -->
+    <script src={{ asset('assets/js/main.js') }}></script>
+</body>
+</html>
