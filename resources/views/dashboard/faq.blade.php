b2e7a15f0aa5071e17c91e6ed2379a4fbe6fb626
zakyuf <mochammadzakyz@gmail.com>
2023-08-26 19:36:48 +0700

    feat:modal edit faq, confirm delete faq

--- resources/views/dashboard/faq.blade.php
+++ resources/views/dashboard/faq.blade.php
@@ -99,12 +99,61 @@
                                     <td>{{ $f->question }}</td>
                                     <td>{{ $f->answer }}</td>
                                     <td>
-                                        <a href="{{ route('faq.edit', $f->id) }}" class="btn icon btn-warning m-1"
-                                            title="Edit"><i class="bi bi-pencil-square"></i></a>
-                                        <form action="{{ route('faq.destroy', $f->id) }}" method="post">
+                                        <button type="button" class="btn icon icon-left btn-primary" data-bs-toggle="modal"
+                                            data-bs-target="#editFormModal{{ $f->id }}"><i class="bi bi-pencil-square"></i></button>
+
+                                        <!-- Edit Form Modal -->
+                                        <div class="modal fade text-left" id="editFormModal{{ $f->id }}" tabindex="-1" role="dialog"
+                                            aria-labelledby="faqEditModal{{ $f->id }}" aria-hidden="true">
+                                            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable"
+                                                role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h4 class="modal-title" id="faqeditModal{{ $f->id }}">Edit FAQ </h4>
+                                                        <button type="button" class="close" data-bs-dismiss="modal"
+                                                            aria-label="Close">
+                                                            <i data-feather="x"></i>
+                                                        </button>
+                                                    </div>
+                                                    <form method="POST" action="{{ route('faq.update', $f->id) }}">
+                                                        @method('PUT')
+                                                        @csrf
+                                                        <div class="modal-body">
+                                                            <div class="form-group">
+                                                                <label for="email" class="form-label">Pertanyaan</label>
+                                                                <input name="question" type="text" class="form-control"
+                                                                    placeholder="Pertanyaan" value="{{ $f->question }}">
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label for="name" class="form-label">Jawaban</label>
+                                                                <textarea name="answer" type="text" class="form-control"placeholder="Jawaban" value="{{ $f->answer }}">{{ $f->answer }}</textarea>
+                                                            </div>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-light-secondary"
+                                                                data-bs-dismiss="modal">
+                                                                <i class="bx bx-x d-block d-sm-none"></i>
+                                                                <span class="d-none d-sm-block">Close</span>
+                                                            </button>
+                                                            <button type="submit" type="button"
+                                                                class="btn btn-primary ml-1" data-bs-dismiss="modal">
+                                                                <i class="bx bx-check d-block d-sm-none"></i>
+                                                                <span class="d-none d-sm-block">Submit</span>
+                                                            </button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- Edit Form Modal -->
+
+                                        <form id="delete-form-{{ $f->id }}"
+                                            action="{{ route('faq.destroy', $f->id) }}" method="post">
                                             @csrf
                                             @method('delete')
-                                            <button type="submit" class="btn icon btn-danger m-1">
+                                            <button type="button" class="btn icon btn-danger m-1 delete-btn"
+                                                data-id="{{ $f->id }}"
+                                                onclick="confirmDelete({{ $f->id }})">
                                                 <i class="bi bi-trash"></i>
                                             </button>
                                         </form>
@@ -121,4 +170,24 @@
             </div>
         </section>
     </div>
+    <script>
+        function confirmDelete(id) {
+            Swal.fire({
+                title: 'Anda yakin?',
+                text: 'Data FAQ akan dihapus permanen!',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Ya, Hapus!',
+                cancelButtonText: 'Batal'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // Jika pengguna mengklik "Ya, Hapus!", kirimkan permintaan penghapusan ke server
+                    document.getElementById('delete-form-' + id).submit();
+                }
+            });
+        }
+    </script>
+
 @endsection
