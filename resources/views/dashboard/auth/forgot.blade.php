c0b0b267bb2da75936def20c88e87ba22f35449d
alimurrofid <alimurrofid77@gmail.com>
2023-11-05 15:05:10 +0700

    feat:name in forgot password

--- resources/views/dashboard/auth/forgot.blade.php
+++ resources/views/dashboard/auth/forgot.blade.php
@@ -25,6 +25,18 @@
                 <i class="bi bi-envelope"></i>
             </div>
         </div>
+        <div class="form-group position-relative has-icon-left mb-4">
+            <input name="name" type="text" class="form-control form-control-xl @error('name') is-invalid @enderror"
+                placeholder="Name" value="{{old('name')}}" autofocus>
+            @error('name')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+            <div class="form-control-icon">
+                <i class="bi bi-person"></i>
+            </div>
+        </div>
         <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Send</button>
     </form>
     <div class="text-center mt-5 text-lg fs-4">
