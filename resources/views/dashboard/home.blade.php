7093e0354cced479cffe50de1bd662a43486fef8
alimurrofid <alimurrofid77@gmail.com>
2023-11-19 19:23:15 +0700

    fix:count article view

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -72,7 +72,7 @@
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="text-muted font-semibold">Article</h6>
-                                        <h6 class="font-extrabold mb-0">1{{$total_article}}</h6>
+                                        <h6 class="font-extrabold mb-0">{{$total_article}}</h6>
                                     </div>
                                 </div>
                             </div>
